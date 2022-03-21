f0f34cc8f295d3db84f7068b96458af4c03c7eee
Evan Walter <evan.walter.software@gmail.com>
2022-03-21 16:40:33 -0700

    feat: rotate left or right based on user input

--- your-first-script/Sprite.gd
+++ your-first-script/Sprite.gd
@@ -5,6 +5,13 @@ var angular_speed = PI
 var speed = 400.0
 
 func _process(delta):
-	rotation += PI * delta
-	var velocity = Vector2.UP.rotated(rotation) * speed
-	position += velocity * delta
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+	
+	rotation += angular_speed * direction * delta
+	
+#	var velocity = Vector2.UP.rotated(rotation) * speed
+#	position += velocity * delta
