33c45368bacf5166606593969f38001aafce1a10
Evan Walter <evan.walter.software@gmail.com>
2022-04-11 17:07:41 -0700

    feat: toggle motion

--- using-signals/ToggleMotion/Godot.gd
+++ using-signals/ToggleMotion/Godot.gd
@@ -8,3 +8,7 @@ func _process(delta):
 	rotation += angular_speed * delta
 	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
+
+
+func _on_Button_pressed():
+	set_process(not is_processing())
