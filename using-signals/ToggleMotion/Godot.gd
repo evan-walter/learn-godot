8333e204f98a8c2041c43aa973c8ceddc474795e
Evan Walter <evan.walter.software@gmail.com>
2022-04-18 17:12:10 -0700

    wip: using signals

--- using-signals/ToggleMotion/Godot.gd
+++ using-signals/ToggleMotion/Godot.gd
@@ -3,6 +3,10 @@ extends Sprite
 var speed = 400
 var angular_speed = PI
 
+func _ready():
+	var timer = get_node("Timer")
+	timer.connect("timeout", self, "_on_Timer_timeout")
+
 
 func _process(delta):
 	rotation += angular_speed * delta
@@ -12,3 +16,6 @@ func _process(delta):
 
 func _on_Button_pressed():
 	set_process(not is_processing())
+	
+func _on_Timer_timeout():
+	visible = not visible
